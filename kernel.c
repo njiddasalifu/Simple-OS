c55e1036ed1e74f27234fb47a0c527ff0e743621
njiddasalifu <njiddasalifu@gmail.com>
2023-01-05 13:21:13 +0100

    project folder named iknowOS is added

---
+++ kernel.c
@@ -0,0 +1,31 @@
+#include "include/screen.h"
+#include "include/kb.h"
+#include "include/string.h"
+void kmain()
+{
+       clearScreen();
+       print("Welcome to our simple operating system\nPlease enter a command\n\n");
+       print("Some commands are bad commands, Please do avoid them.\n");
+       while (1)
+       {
+                print("\nSimple OS> ");
+                
+                string ch = readStr();
+                if(strEql(ch,"cmd"))
+                {
+                        print("\nYou are allready in command prompt\n");
+                }
+                else if(strEql(ch,"clear"))
+                {
+                        clearScreen();
+                }
+                
+                else
+                {
+                        print("\nBad command!\n");
+                }
+                
+                print("\n");        
+       }
+        
+}
